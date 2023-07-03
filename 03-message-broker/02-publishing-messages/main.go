3bdb1f307d0c7193374eefcb08e01d496cd39a20
Diego Maia <diegocmsantos@gmail.com>
2023-07-03 23:03:59 +0100

    publishing messages

---
+++ 03-message-broker/02-publishing-messages/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	logger := watermill.NewStdLogger(false, false)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	publisher, err := redisstream.NewPublisher(redisstream.PublisherConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+	msg50 := message.NewMessage(watermill.NewUUID(), []byte("50"))
+	msg100 := message.NewMessage(watermill.NewUUID(), []byte("100"))
+	publisher.Publish("progress", msg50)
+	publisher.Publish("progress", msg100)
+}
