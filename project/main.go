1236d5eb2ad8f6b2bf8a7f69834ec70ab1715912
Diego Maia <diegomaia@Diegos-MBP.home>
2023-07-03 22:39:55 +0100

    asynchronous done

--- project/main.go
+++ project/main.go
@@ -22,13 +22,8 @@ type TicketsConfirmationRequest struct {
 func main() {
 	log.Init(logrus.InfoLevel)
 
-	clients, err := clients.NewClients(os.Getenv("GATEWAY_ADDR"), nil)
-	if err != nil {
-		panic(err)
-	}
-
-	receiptsClient := NewReceiptsClient(clients)
-	spreadsheetsClient := NewSpreadsheetsClient(clients)
+	w := NewWorker()
+	go w.Run()
 
 	e := commonHTTP.NewEcho()
 
@@ -40,15 +35,16 @@ func main() {
 		}
 
 		for _, ticket := range request.Tickets {
-			err = receiptsClient.IssueReceipt(c.Request().Context(), ticket)
-			if err != nil {
-				return err
-			}
-
-			err = spreadsheetsClient.AppendRow(c.Request().Context(), "tickets-to-print", []string{ticket})
-			if err != nil {
-				return err
-			}
+			w.Send(
+				Message{
+					Task:     TaskIssueReceipt,
+					TicketID: ticket,
+				},
+				Message{
+					Task:     TaskAppendToTracker,
+					TicketID: ticket,
+				},
+			)
 		}
 
 		return c.NoContent(http.StatusOK)
@@ -56,7 +52,7 @@ func main() {
 
 	logrus.Info("Server starting...")
 
-	err = e.Start(":8080")
+	err := e.Start(":8080")
 	if err != nil && err != http.ErrServerClosed {
 		panic(err)
 	}
@@ -112,4 +108,62 @@ func (c SpreadsheetsClient) AppendRow(ctx context.Context, spreadsheetName strin
 	}
 
 	return nil
+}
+
+type Task int
+
+const (
+	TaskIssueReceipt = iota
+	TaskAppendToTracker
+)
+
+type Message struct {
+	Task     Task
+	TicketID string
+}
+
+type Worker struct {
+	queue chan Message
+}
+
+func NewWorker() *Worker {
+	return &Worker{
+		queue: make(chan Message, 100),
+	}
+}
+
+func (w *Worker) Send(msg ...Message) {
+	for _, m := range msg {
+		w.queue <- m
+	}
+}
+
+func (w *Worker) Run() {
+
+	clients, err := clients.NewClients(os.Getenv("GATEWAY_ADDR"), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	receiptsClient := NewReceiptsClient(clients)
+	spreadsheetsClient := NewSpreadsheetsClient(clients)
+
+	for msg := range w.queue {
+		switch msg.Task {
+		case TaskIssueReceipt:
+			// issue the receipt
+			err = receiptsClient.IssueReceipt(context.Background(), msg.TicketID)
+			if err != nil {
+				logrus.WithError(err).Error("failed to issue the receipt")
+				w.Send(msg)
+			}
+		case TaskAppendToTracker:
+			// append to the tracker spreadsheet
+			err = spreadsheetsClient.AppendRow(context.Background(), "tickets-to-print", []string{msg.TicketID})
+			if err != nil {
+				logrus.WithError(err).Error("failed to append to tracker")
+				w.Send(msg)
+			}
+		}
+	}
 }
