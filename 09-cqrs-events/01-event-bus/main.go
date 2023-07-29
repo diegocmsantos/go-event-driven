12440a1868f25a10330470aa370876a14e03d931
Diego Maia <diegocmsantos@gmail.com>
2023-07-29 17:06:14 +0100

    Project: Using the Event Bus

--- 09-cqrs-events/01-event-bus/main.go
+++ 09-cqrs-events/01-event-bus/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ThreeDotsLabs/watermill/components/cqrs"
 	"github.com/ThreeDotsLabs/watermill/message"
 )
 
 func NewEventBus(pub message.Publisher) (*cqrs.EventBus, error) {
-	return nil, fmt.Errorf("not implemented")
+	cfg := cqrs.EventBusConfig{
+		GeneratePublishTopic: func(params cqrs.GenerateEventPublishTopicParams) (string, error) {
+			return params.EventName, nil
+		},
+		Marshaler: cqrs.JSONMarshaler{},
+	}
+	return cqrs.NewEventBusWithConfig(pub, cfg)
 }
