09a5cf9b739fa4bac9c4a4d1fd7ed197d58e83f9
Diego Maia <diegocmsantos@gmail.com>
2023-07-29 17:11:50 +0100

    Project: Custom Event Name

---
+++ 09-cqrs-events/02-custom-event-name/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+func NewEventBus(pub message.Publisher) (*cqrs.EventBus, error) {
+	cfg := cqrs.EventBusConfig{
+		GeneratePublishTopic: func(params cqrs.GenerateEventPublishTopicParams) (string, error) {
+			return params.EventName, nil
+		},
+		Marshaler: cqrs.JSONMarshaler{
+			GenerateName: cqrs.StructName,
+		},
+	}
+	return cqrs.NewEventBusWithConfig(pub, cfg)
+}
